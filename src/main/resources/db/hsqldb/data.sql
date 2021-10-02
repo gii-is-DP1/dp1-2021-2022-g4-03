4332b6e4969b9247f599ddd123efb3234a544a18
RafaJF <61872929+RafaJF@users.noreply.github.com>
2021-10-03 00:22:36 +0200

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -9,8 +9,10 @@ INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
 
 -- Inserting serrivroa to users table and giving role 'Owner'
+INSERT INTO users(username, password, enabled) VALUES ('rafjimfer', 'default', TRUE);
+INSERT INTO authorities(id, username, authority) VALUES (4, 'rafjimfer', 'owner');
 INSERT INTO users(username, password, enabled) VALUES ('serrivroa', 'default', TRUE);
-INSERT INTO authorities(id, username, authority) VALUES (4, 'serrivroa', 'owner');
+INSERT INTO authorities(id, username, authority) VALUES (5, 'serrivroa', 'owner');
 
 INSERT INTO vets VALUES (1, 'James', 'Carter');
 INSERT INTO vets VALUES (2, 'Helen', 'Leary');
@@ -46,7 +48,7 @@ INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
-INSERT INTO owners VALUES (11, 'rafjimfer','J', 'Calle Falsa 123', 'Sevilla', '696969696', 'owner1');
+INSERT INTO owners VALUES (11, 'Rafael Ángel','Jiménez Fernández', 'Calle Falsa 123', 'Sevilla', '696969696', 'rafjimfer');
 INSERT INTO owners VALUES (12, 'Sergio', 'Rivas Roa', 'Avenida de Ferrocarril', 'Monopoly', '123456789', 'serrivroa');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
