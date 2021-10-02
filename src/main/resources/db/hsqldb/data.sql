d5697d343266b295eb7dd38340fa3e1071d4d049
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2021-10-02 17:59:58 +0200

    Updated data.sql
    Added queries to sql database to reflect serrivroa developer addition.

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -8,6 +8,10 @@ INSERT INTO authorities(id,username,authority) VALUES (2,'owner1','owner');
 INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
 
+-- Inserting serrivroa to users table and giving role 'Owner'
+INSERT INTO users(username, password, enabled) VALUES ('serrivroa', 'default', TRUE);
+INSERT INTO authorities(id, username, authority) VALUES (4, 'serrivroa', 'owner');
+
 INSERT INTO vets VALUES (1, 'James', 'Carter');
 INSERT INTO vets VALUES (2, 'Helen', 'Leary');
 INSERT INTO vets VALUES (3, 'Linda', 'Douglas');
@@ -43,6 +47,7 @@ INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
 INSERT INTO owners VALUES (11, 'rafjimfer','J', 'Calle Falsa 123', 'Sevilla', '696969696', 'owner1');
+INSERT INTO owners VALUES (12, 'Sergio', 'Rivas Roa', 'Avenida de Ferrocarril', 'Monopoly', '123456789', 'serrivroa');
 
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
@@ -58,6 +63,7 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Coco', '2018-06-08', 2, 11);
+INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (15, 'Gatete', '2021-06-08', 1, 12);
 
 
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
