031fabbca53f5839854e96ff8be9ead02159d15a
RafaJF <61872929+RafaJF@users.noreply.github.com>
2021-10-28 20:57:44 +0200

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,9 +1,7 @@
 
-
 INSERT INTO users(nickname, email, pass, active) VALUES ('owner1','5678@test.com','0wn3r',TRUE);
 INSERT INTO users(nickname, email, pass, active) VALUES ('vet1','vet@test.com','v3t',TRUE);
 INSERT INTO users(nickname, email, pass, active) VALUES ('rafjimfer','raf@test.com', 'rafa', TRUE);
 INSERT INTO users(nickname, email, pass, active) VALUES ('serrivroa','ser@test.com', 'default', TRUE);
-
 
 
