64df850e8c052c9cc4b0dfc3b2f467c639eeae22
RafaJF <61872929+RafaJF@users.noreply.github.com>
2021-10-28 20:27:56 +0200

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -1,10 +1,7 @@
 
--- One owner user, named owner1 with passwor 0wn3r
-INSERT INTO users(nickname, email, pass, active) VALUES ('owner1','5678@test.com','0wn3r',TRUE);
--- One vet user, named vet1 with passwor v3t
-INSERT INTO users(nickname, email, pass, active) VALUES ('vet1','vet@test.com','v3t',TRUE);
 
--- Inserting serrivroa to users table and giving role 'Owner'
+INSERT INTO users(nickname, email, pass, active) VALUES ('owner1','5678@test.com','0wn3r',TRUE);
+INSERT INTO users(nickname, email, pass, active) VALUES ('vet1','vet@test.com','v3t',TRUE);
 INSERT INTO users(nickname, email, pass, active) VALUES ('rafjimfer','raf@test.com', 'rafa', TRUE);
 INSERT INTO users(nickname, email, pass, active) VALUES ('serrivroa','ser@test.com', 'default', TRUE);
 
