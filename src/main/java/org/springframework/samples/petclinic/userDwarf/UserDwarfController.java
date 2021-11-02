24b6c706e3f2b9baecf7791c1ab516d9f062f411
RafaJF <61872929+RafaJF@users.noreply.github.com>
2021-11-02 15:35:22 +0100

    Solución del error de null
    Se ha borrado la entidad admin ya que no hace falta porque es un usuario con permisos especiales. Se ha creado una entidad, controlador, servicio , repositorio y vista de lista de usuario. No se ha logrado que se muestre la vista.

---
+++ src/main/java/org/springframework/samples/petclinic/userDwarf/UserDwarfController.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.userDwarf;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/userDwarf")
+public class UserDwarfController {
+
+    @Autowired
+    private UserDwarfService userDwarfService; 
+
+    @InitBinder
+	public void setAllowedFields(WebDataBinder dataBinder) {
+		dataBinder.setDisallowedFields("id");
+	}
+
+    @GetMapping("/userDwarf/list")
+    public String UserDwarfList(ModelMap modelMap){
+        
+        String view = "/usersDwarf/userDwarfList";
+        Iterable<UserDwarf> usersDwarf = userDwarfService.findAll();
+        modelMap.addAttribute("usersDwarf",usersDwarf); 
+        
+        return view;
+
+    }
+    
+}
