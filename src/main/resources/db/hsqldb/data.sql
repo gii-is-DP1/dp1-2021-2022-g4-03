ee363079b2b0a1ab4de6f67745c46f26e48f8c45
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2021-11-25 22:53:51 +0100

    Update data.sql

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -16,3 +16,7 @@ INSERT INTO authorities(id,username, authority) VALUES (3, 'frabotrom', 'admin')
 INSERT INTO achievements(id,last_change,condition,description) VALUES (1,'2022-01-02','total_gold=100','Consigue 100 de hierro');
 INSERT INTO achievements(id,last_change,condition,description) VALUES (2,'2022-01-02','total_iron=300','Consigue 100 de oro');
 INSERT INTO achievements(id,last_change,condition,description) VALUES (3,'2022-01-02','total_games=1','Consigue 100 de acero');
+
+-- Time Played is expressed in nanoseconds (10^-9)
+INSERT INTO statistics (time_played, games_played, games_won, total_iron, total_gold, total_steel, total_object, total_medal, user_dwarf_id) VALUES ('58243000000000', 8, 5, 150, 100, 75, 28, 15, 'rafjimfer');
+INSERT INTO statistics (time_played, games_played, games_won, total_iron, total_gold, total_steel, total_object, total_medal, user_dwarf_id) VALUES ('24745000000000', 7, 4, 120, 90, 77, 31, 18, 'serrivroa');
