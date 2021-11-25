a20554a519198ff597178b233bacc7d8c827000b
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2021-11-25 19:28:48 +0100

    data.sql fix

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -2,9 +2,9 @@ INSERT INTO users(username,password,enabled) VALUES ('admin1','4dm1n',TRUE);
 INSERT INTO users(username,password,enabled) VALUES ('owner1','0wn3r',TRUE);
 INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
 
-INSERT INTO usersdwarf(username, pass,email, active) VALUES ('rafjimfer', 'default','rafa@gmail.com', TRUE);
-INSERT INTO usersdwarf(username, pass,email, active) VALUES ('serrivroa', 'default','ss@gmail.com', TRUE);
 
--- Inserting test game and board --
--- INSERT INTO board() -- 
--- INSERT INTO game(player1, player2, player3, order, phase, gameStatus, board) VALUES ('rafjimfer', 'serrivroa', null, [1,2,3], 'INICIO', 'IN_PROGRESS', 1) --
+
+INSERT INTO usersdwarf(username, pass,email, active) VALUES ('rafjimfer', 'rafa','rafa@gmail.com', TRUE);
+INSERT INTO authorities(id,username, authority) VALUES (1, 'rafjimfer', 'admin');
+INSERT INTO usersdwarf(username, pass,email, active) VALUES ('serrivroa', 'sergio','ss@gmail.com', TRUE);
+INSERT INTO authorities(id,username, authority) VALUES (2, 'serrivroa', 'admin');
