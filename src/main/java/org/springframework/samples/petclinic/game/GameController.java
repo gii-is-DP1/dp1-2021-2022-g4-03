7d572f24ad4b2c56e26bf6fdd28e17dc51cb29eb
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2021-12-04 19:29:48 +0100

    Update

--- src/main/java/org/springframework/samples/petclinic/game/GameController.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.board.BoardService;
 import org.springframework.samples.petclinic.userDwarf.UserDwarf;
 import org.springframework.samples.petclinic.userDwarf.UserDwarfService;
+import org.springframework.samples.petclinic.web.CurrentUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,20 +53,23 @@ public class GameController {
 	@GetMapping(value = "/game/new")
 	public String createGame() {
 		// Hasta que no tengamos currentUser creamos la partida con el user frabotrom
-		Game game=gameService.createGame(userDwarfService.findById(3));
+		UserDwarf player= userDwarfService.findUserDwarfByUsername2(CurrentUser.getCurrentUser()).get();
+		Game game=gameService.createGame(player);
 		return "redirect:/board/"+game.getId();
 	}
 
 	@GetMapping(value = "/game/connect/{gameId}")
 	public String connectToGame(@PathVariable("gameId") Integer gameId) {
 		// Hasta que no tengamos currentUser conectamos a un user random
-		gameService.connectToGame(userDwarfService.findById(1), gameId);
+		UserDwarf player= userDwarfService.findUserDwarfByUsername2(1);
+		gameService.connectToGame(player, gameId);
 		return "redirect:/board/{gameId}";
 	}
 
 	@GetMapping(value = "/game/{gameId}/surrender")
 	public String surrender(@PathVariable("gameId") Integer gameId) {
-		gameService.surrender(gameId, userDwarfService.findById(1));
+		UserDwarf player= userDwarfService.findUserDwarfByUsername2(CurrentUser.getCurrentUser()).get();
+		gameService.surrender(gameId, player);
 		return "redirect:/";
 	}
 
