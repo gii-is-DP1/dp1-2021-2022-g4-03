e5d03fd3929c8df879366d56cc6d53887981a34e
Fran Botello <47320585+frabotrom@users.noreply.github.com>
2021-12-04 19:30:18 +0100

    Card created
    Empty cards

---
+++ src/main/java/org/springframework/samples/petclinic/card/CardFactory.java
@@ -0,0 +1,5 @@
+package org.springframework.samples.petclinic.card;
+
+public class CardFactory {
+
+}
---
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -0,0 +1,12 @@
+package org.springframework.samples.petclinic.card;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CardService {
+    
+    @Autowired
+    CardRepository cardRepository;
+
+    
+
+}
