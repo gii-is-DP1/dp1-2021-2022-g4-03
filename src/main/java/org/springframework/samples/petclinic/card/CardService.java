66570a59bcae61dcf44472ea1775a87321d0fa84
Fran Botello <47320585+frabotrom@users.noreply.github.com>
2021-12-08 16:52:36 +0100

    cardFactory planteado

--- src/main/java/org/springframework/samples/petclinic/card/CardService.java
+++ src/main/java/org/springframework/samples/petclinic/card/CardService.java
@@ -1,12 +1,29 @@
 package org.springframework.samples.petclinic.card;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 public class CardService {
     
     @Autowired
     CardRepository cardRepository;
 
+    private void cardEffectConstructor (String effect) {
+        // Card(String effect) -> Card(Predicate effect)
+    }
+
+//    @Transactional
+//    public List<Card> getAllCards() {
+//        Stream<Card> str = StreamSupport.stream(cardRepository.findAll().spliterator(), false);
+//        return str.map(Card::getEffect)
+//            .forEach(x->cardEffectConstructor(x))
+//            .collect(Collectors.toList());
+//    }
     
 
 }
