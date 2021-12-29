4ee0790ed8ffee7b9edb55419ac3893ac6891eb3
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2021-12-29 19:08:44 +0100

    Board background succesfully draws on being loaded. TBD: display background with less opacity.

--- src/main/resources/static/resources/js/board.js
+++ src/main/resources/static/resources/js/board.js
@@ -4,12 +4,26 @@ function main(){
     let form = document.getElementById("testForm");
 
     form.addEventListener("submit", handleClick, false);
+
+    window.addEventListener("load", loadBackground, false);
+
 }
 
 function handleClick(event) {
     event.preventDefault();
     console.log("Works");
-    
+
 }
+
+function loadBackground(event){
+    console.log("This is triggering load");
+    let canvas = document.getElementById("canvas");
+
+    let ctx = canvas.getContext("2d");
+    let image = document.getElementById('boardBackground');
+
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
+
 
 document.addEventListener("DOMContentLoaded", main, false);
