3c2b2d17280befabddeba9523cfe1739cef979fc
Fran Botello <47320585+frabotrom@users.noreply.github.com>
2022-01-02 10:45:15 +0100

    Minor code smells erased and card images uploaded

--- src/test/java/org/springframework/samples/petclinic/testUserDwarf/testUserDwarfService.java
+++ src/test/java/org/springframework/samples/petclinic/testUserDwarf/testUserDwarfService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
