97156dc1f21bb17029ea90c8ff6896f5263c9be0
Sergio Rivas Roa <sergiorivasroa@gmail.com>
2022-01-06 20:09:59 +0100

    Minor changes

--- src/test/java/org/springframework/samples/petclinic/testBoard/testBoardController.java
+++ src/test/java/org/springframework/samples/petclinic/testBoard/testBoardController.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.board.BoardController;
 import org.springframework.samples.petclinic.card.Card;
-import org.springframework.samples.petclinic.card.CardController;
 import org.springframework.samples.petclinic.card.CardService;
 import org.springframework.samples.petclinic.card.CardType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
@@ -20,9 +19,6 @@ import org.springframework.context.annotation.FilterType;
 public class testBoardController {
 
     private static final int TEST_CARD_ID = 1;
-
-	@Autowired
-	private CardController cardController;
 
 	@MockBean
 	private CardService cardService;
