89bc25e716b3140c138cd9c3a4be8771ae09b43f
suarez0552 <61872579+suarez0552@users.noreply.github.com>
2022-01-21 20:46:58 +0100

    test modificados y creados

--- src/main/java/org/springframework/samples/petclinic/statistics/StatisticsService.java
+++ src/main/java/org/springframework/samples/petclinic/statistics/StatisticsService.java
@@ -168,7 +168,14 @@ public class StatisticsService {
     public Long getMaxTimePlayed(){
         return this.statisticsRepository.getMaxTimePlayed()/1000000000 ;
     }
+<<<<<<< Updated upstream
     
+=======
+    @Transactional(readOnly = true)
+    public Long getUserMinGold(String username){
+        return this.statisticsRepository.getMinGold(username);
+    }
+>>>>>>> Stashed changes
     
     @Transactional
     public void updateStatistics(PlayerState playerState, Statistics statistics, boolean winner) {
